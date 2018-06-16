2c0ed6e4c1225b42a29c8a83ecd879bd8dc96293
Virunee <viravongv@gmail.com>
2018-06-16 17:56:12 +0000

    Pass JSON and rearrange structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import treedata from './treedata'
+import SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: treedata
+    };
+  }
+  
   render() {
+
+    var arr = [];
+    Object.keys(treedata).forEach(function(key) {
+      arr.push(treedata[key]);
+    });
+
+  function convert(array){
+    var map = {};
+    for(var i = 0; i < array.length; i++){
+        var obj = array[i];
+        obj.children= [];
+
+        map[obj.id] = obj;
+
+        var parent = obj.parentId || '-';
+        if(!map[parent]){
+            map[parent] = {
+              children: []
+            };
+        }
+        map[parent].children.push(obj);
+    }
+
+    return map['-'].children;
+
+}
+
+var r = convert(arr);
+
+const objects = r.map(
+        (object) => 
+    <li key={object.id}>{object.title}</li>
+    );
+  
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +58,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{objects}</p>
       </div>
     );
   }
-}
+};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function todos(state = [], action) {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return state.concat([action.text])
+      default:
+        return state
+    }
+  };
+
+const store = createStore(todos);
+
+store.dispatch({
+    type: 'ADD_TODO',
+    text: 'Read the docs'
+  });
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
